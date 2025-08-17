9139c4a97782ea9544ca41ec9a06e2655ac97a8e
Sanket R <136331865+cutaneous@users.noreply.github.com>
2025-08-17 23:19:35 +0530

    Create kafkaconsumerconfig.java
    src/main/java/com/jpmc/midascore/component/KafkaConsumerConfig.java

---
+++ src/main/java/com/jpmc/midascore/component/kafkaconsumerconfig.java
@@ -0,0 +1,28 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Transaction> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:9092"); // Will be auto-wired for tests
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "midas-core-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(),
+                new JsonDeserializer<>(Transaction.class, false));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Transaction> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Transaction> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
